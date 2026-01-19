d458a8fca087905562ec8c79812be4a21e233e9f
Claudio Olivares <ca.olivares91@gmail.com>
2026-01-19 20:32:32 -0300

    feat se limpia proyecto

--- src/actions/products/index.ts
+++
@@ -1,5 +0,0 @@
-
-
-export * from './get-product-by-id';
-export * from './get-products-by-page';
-export * from './update-create-product';
--- src/domain/entities/product.ts
+++ src/domain/entities/product.ts
@@ -1,31 +1,28 @@
-
-
 export interface Product {
-  id:          string;
-  title:       string;
-  price:       number;
+  id: string;
+  title: string;
+  price: number;
   description: string;
-  slug:        string;
-  stock:       number;
-  sizes:       Size[];
-  gender:      Gender;
-  tags:        string[];
-  images:      string[];
+  slug: string;
+  stock: number;
+  sizes: Size[];
+  gender: Gender;
+  tags: string[];
+  images: string[];
 }
 
 export enum Gender {
-  Kid = "kid",
-  Men = "men",
-  Unisex = "unisex",
-  Women = "women",
+  Kid = 'kid',
+  Men = 'men',
+  Unisex = 'unisex',
+  Women = 'women',
 }
 
 export enum Size {
-  L = "L",
-  M = "M",
-  S = "S",
-  Xl = "XL",
-  Xs = "XS",
-  Xxl = "XXL",
+  L = 'L',
+  M = 'M',
+  S = 'S',
+  Xl = 'XL',
+  Xs = 'XS',
+  Xxl = 'XXL',
 }
-
